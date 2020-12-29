59ed29180c284d1ee20cf92d8ddc9cea682f49bb
Sóstenes <sostenesapollo25@gmail.com>
2020-12-28 23:54:05 -0300

    Using Classes

--- app/urls.py
+++ app/urls.py
@@ -1,8 +1,11 @@
 from django.urls import path
 
-from app.views import todo_list, todo_detail_change_and_delete
+# from app.views import todo_list, todo_detail_change_and_delete, TodoListAndCreate
+from app.views import TodoListAndCreate,TodoDetailChangeAndDelete   
 
 urlpatterns = [
-    path('', todo_list),
-    path('<int:pk>/', todo_detail_change_and_delete),
+    # path('', todo_list),
+    # path('<int:pk>/', todo_detail_change_and_delete),
+    path('', TodoListAndCreate.as_view()),
+    path('<int:pk>/', TodoDetailChangeAndDelete.as_view())
 ]
